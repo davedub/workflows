25a931c0ef563742e9ec2f8299a1b454240bb0a6
David Witus <imac_home@iMac27.local>
2017-01-31 15:44:41 -0800

    add gulp concat js method and output

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 // tasks that are run when the gulp command is issued
 // thereby triggering gulp
@@ -13,9 +14,24 @@ var gulp = require('gulp'),
 
 var coffeeSources = ['components/coffee/*.coffee']
 
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
+
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 	.pipe(coffee({bare: true})
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function() {
+	gulp.src(jsSources) // source
+		.pipe(concat('script.js')) // output
+		.pipe(gulp.dest('builds/development/js'))
+});
+
+
