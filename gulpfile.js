c1f123bbbfdf04d4ee5b03dc6583633636f6f877
David Witus <dwitus@gmail.com>
2017-01-30 20:38:53 -0800

    add coffeescript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
-var 	gulp = require('gulp'),
-	gutil = require('gulp-util');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
 // tasks that are run when the gulp command is issued
 // thereby triggering gulp
 // are anonymous functions
 
-gulp.task('log', function () {
-	// use gutil variable
-	gutil.log("Workflows are super awesome");
+// gulp.task('log', function () {
+// 	// use gutil variable
+// 	gutil.log("Workflows are super awesome");
+// });
+
+var coffeeSources = ['components/coffee/*.coffee']
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSources)
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
