f9506bbf17283b80800f09fd40f58fc86ca6c0f9
David Witus <dwitus@gmail.com>
2017-02-06 10:23:01 -0800

    add tasks for autoreload of static files html and json

--- gulpfile.js
+++ gulpfile.js
@@ -59,20 +59,33 @@ gulp.task('rubysass', function(){
 
 });
 
-gulp.task('watch', function() {
-	gulp.watch(coffeeSources, ['coffee']);
-	gulp.watch(jsSources, ['js']);
- 	gulp.watch('components/sass/*.scss', ['rubysass']);
-});
-
 gulp.task('connect', function() {
 	connect.server({
 		root: 'builds/development',
 		livereload: true
 	})
 });
+var htmlSources = ['builds/development/*.html']
 
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+});  
+
+var jsonSources = ['builds/development/js/*.json']
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+	.pipe(connect.reload())
+});  
+
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+ 	gulp.watch('components/sass/*.scss', ['rubysass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
+});
 
 gulp.task('default', 
-	['coffee', 'js', 'rubysass', 'connect', 'watch']);
-
+	['html', 'json', 'coffee', 'js', 'rubysass', 'connect', 'watch']);
